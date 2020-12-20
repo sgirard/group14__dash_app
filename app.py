88881b021d486d95a6e616f6d99a550b02bb429c
Scott Girard <sgirard@uncc.edu>
2020-12-20 11:05:54 -0500

    add layout to app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, serve_locally=True)
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
 server = app.server
